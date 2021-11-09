2cca80d91b2b8f82d0ccbdbf517d42f1038b1a3c
RetroBoi128 <64989222+RetroDude128@users.noreply.github.com>
2021-11-08 22:22:53 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,217 @@
-
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . 1 1 f f f f f 1 . . . . 
+        . . . . f f f 1 1 1 f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . f f f f f 1 1 1 . . . . 
+        . . . . f 1 1 1 f f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . f f f 1 1 1 f f . . . . 
+        . . . . 1 1 f f f f f 1 . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . f 1 1 1 f f f f . . . . 
+        . . . . f f f f f 1 1 1 . . . . 
+        `],
+    50,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . c f . . . . . . . 
+        . . . . . . . f c . . . . . . . 
+        . . . . f f f . . f f f . . . . 
+        `)
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+    mySprite.setImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . f 1 f f 1 f f 1 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f c . . . . . . . 
+        . . . . . . . c f . . . . . . . 
+        . . . . f f f . . f f f . . . . 
+        `)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . 1 f f f f f 1 1 . . . . 
+        . . . . f f 1 1 1 f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . 1 1 1 f f f f f . . . . 
+        . . . . f f f f 1 1 1 f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . f f 1 1 1 f f f . . . . 
+        . . . . 1 f f f f f 1 1 . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f 1 1 f 1 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . f 1 1 f f 1 f f 1 f . . . 
+        . . . . f 1 1 1 1 1 1 f . . . . 
+        . . . . . f 1 1 1 1 f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . f f f f 1 1 1 f . . . . 
+        . . . . 1 1 1 f f f f f . . . . 
+        `],
+    50,
+    true
+    )
+})
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . . f 1 1 1 1 f . . . . . 
+    . . . . f 1 1 1 1 1 1 f . . . . 
+    . . . f 1 1 f f 1 f f 1 f . . . 
+    . . . f 1 1 f 1 1 f 1 1 f . . . 
+    . . . f 1 1 f f 1 f f 1 f . . . 
+    . . . f 1 1 f f 1 f f 1 f . . . 
+    . . . . f 1 1 1 1 1 1 f . . . . 
+    . . . . . f 1 1 1 1 f . . . . . 
+    . . . . . . f f f f . . . . . . 
+    . . . . . . . c f . . . . . . . 
+    . . . . . . . f c . . . . . . . 
+    . . . . f f f . . f f f . . . . 
+    `, SpriteKind.Player)
+tiles.setTilemap(tilemap`level2`)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
---
+++ tilemap.g.ts
@@ -0,0 +1,74 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000030000000000000000000000000000000300000000000000000000000100000003000100000000000000000003000000030003000000000000000000040000000400040000050505000202020202020202020202020202020202020202020202020202020202020202`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,myTiles.tile1,sprites.castle.tilePath5,myTiles.tile2,myTiles.tile3,myTiles.tile4], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile1":
+            case "tile3":return tile3;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
